9ecde8af1e41c8004b4ddb6efbb9d8d356ddeef0
김태균 <taekyun0525@gmail.com>
2026-06-09 11:44:03 +0900

    first commit

---
+++ src/main/java/com/example/repo_be_v2/RepoBeV2Application.java
@@ -0,0 +1,13 @@
+package com.example.repo_be_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoBeV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoBeV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/repo_be_v2/RepoBeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repo_be_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoBeV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
