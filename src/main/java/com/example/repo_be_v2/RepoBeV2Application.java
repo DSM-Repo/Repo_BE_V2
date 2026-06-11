1630dbfcabb723da18e1e3fcedc415b283b328c6
Kanghee <kh63167574@gmail.com>
2026-06-11 15:04:01 +0900

    feat : yml 세팅

--- src/main/java/com/example/repo_be_v2/RepoBeV2Application.java
+++ src/main/java/com/example/repo_be_v2/RepoBeV2Application.java
@@ -3,9 +3,11 @@ package com.example.repo_be_v2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableJpaAuditing
 public class RepoBeV2Application {
 
     public static void main(String[] args) {
