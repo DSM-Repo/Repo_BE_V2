0d951003d3c9baf042a93399bed8092ddd9e75ae
Kanghee <kh63167574@gmail.com>
2026-06-11 14:56:33 +0900

    refactor : 어노테이션 추가

--- src/main/java/com/example/repo_be_v2/RepoBeV2Application.java
+++ src/main/java/com/example/repo_be_v2/RepoBeV2Application.java
@@ -2,8 +2,10 @@ package com.example.repo_be_v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class RepoBeV2Application {
 
     public static void main(String[] args) {
