acd72cb15724ca68accc958c1a240303b4b16c6b
김태균 <taekyun0525@gmail.com>
2026-07-22 15:15:29 +0900

    UserController 어노테이션 추가

--- src/main/java/com/example/repo_be_v2/domain/user/presentation/UserController.java
+++ src/main/java/com/example/repo_be_v2/domain/user/presentation/UserController.java
@@ -1,11 +1,13 @@
 package com.example.repo_be_v2.domain.user.presentation;
 
 import com.example.repo_be_v2.domain.user.service.UserLoginService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
     private final UserLoginService userLoginService;
 
