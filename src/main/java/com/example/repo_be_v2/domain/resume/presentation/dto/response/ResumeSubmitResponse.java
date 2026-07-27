15ad971e6e7a0ef426701f344a38626670e0e99d
김태균 <taekyun0525@gmail.com>
2026-07-27 23:01:12 +0900

    Revert "localDatetime 삭제"
    This reverts commit 79af90f8844734d0c7b600d1a5590302a3e5ed8c.

--- src/main/java/com/example/repo_be_v2/domain/resume/presentation/dto/response/ResumeSubmitResponse.java
+++ src/main/java/com/example/repo_be_v2/domain/resume/presentation/dto/response/ResumeSubmitResponse.java
@@ -1,6 +1,9 @@
 package com.example.repo_be_v2.domain.resume.presentation.dto.response;
 
 import com.example.repo_be_v2.domain.resume.domain.enms.ResumeSubmissionStatus;
+
+import java.time.LocalDateTime;
+
 public record ResumeSubmitResponse(
         String resumeId,
         ResumeSubmissionStatus submissionStatus
