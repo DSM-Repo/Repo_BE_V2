58048ba79f70ea8403f6e8b6f592d86730f178a0
Kanghee <kh63167574@gmail.com>
2026-08-28 09:02:28 +0900

    feat : cors 세팅

--- src/main/java/com/example/repo_be_v2/RepoBeV2Application.java
+++ src/main/java/com/example/repo_be_v2/RepoBeV2Application.java
@@ -1,12 +1,13 @@
 package com.example.repo_be_v2;
 
+import com.example.repo_be_v2.global.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigurationProperties(CorsProperties.class)
 @EnableJpaAuditing
 public class RepoBeV2Application {
 
